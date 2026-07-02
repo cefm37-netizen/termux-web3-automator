476b2c86f33edf9f4aa75949ea39c0feadc544e0
cefm37 <cefm37@hotmail.com>
2026-07-02 18:06:28 +0300

    fix: key-backup betigi guncellendi, monad-check yeniden baslatildi

--- core/backup_keys.sh
+++ core/backup_keys.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir -p /data/data/com.termux/files/home/storage/shared/backup_keys
 cp /data/data/com.termux/files/home/termux-web3-automator/keys.json.enc /data/data/com.termux/files/home/storage/shared/backup_keys/keys.json.enc
 echo "[$(date)] Yedek alındı" >> ~/backup.log
